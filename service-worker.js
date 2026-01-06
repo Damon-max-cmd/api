23df4165a2e276b426c92ca1bfeac21ced724458
Damon-max-cmd <c8h8xhvw4t@privaterelay.appleid.com>
2026-01-06 15:56:41 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE = "inazuma-v1";
+const CACHE = "Damon-v1";
 const ASSETS = [
   "/",
   "/pair",
   "/manifest.json",
   "/service-worker.js",
-  "https://files.catbox.moe/cuqc9o.jpg",
+  "https://files.catbox.moe/57tf3e.jpeg",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css",
   "https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;800&display=swap"
 ];
